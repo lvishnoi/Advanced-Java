54621126a93126aa0fef1393a377c714fa04f5ef
Lokesh Vishnoi <lokeshvishnoi1@gmail.com>
2020-09-15 22:31:28 +0530

    Maven project

---
+++ HelloWorldEclipse/src/main/java/com/kesho/maven/HelloWorldEclipse/App.java
@@ -0,0 +1,13 @@
+package com.kesho.maven.HelloWorldEclipse;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
